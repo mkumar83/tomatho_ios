0842a1ba38608f40d4e3b9df9b756ca2f1a60d98
mchinnu <muthukumar.chinnu@amadeus.com>
2017-10-25 22:26:21 +0530

    Initial App

---
+++ Tomatho/Tomatho/LaunchScreen.swift
@@ -0,0 +1,59 @@
+//
+//  LaunchScreen.swift
+//  Tomatho
+//
+//  Created by Muthukumar Chinnu on 25/06/17.
+//  Copyright © 2017 Tomatho. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreen: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let employees = ["Emp1", "Emp2", "Emp3"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return employees.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                return UITableViewCell(style: .default, reuseIdentifier: "cell")
+            }
+            return cell
+        }()
+        
+        cell.textLabel?.text = employees[indexPath.row]
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
